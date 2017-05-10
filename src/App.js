956bad8e4435267203309766f42151e4892d30d4
Charles P. Oliver <charles.oliver.extern@holidaycheck.com>
2017-05-10 13:42:15 +0300

    Add props to set the clock time

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Clock />
+        <Clock hours={12} minutes={15} seconds={30} />
       </div>
     );
   }
--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -2,13 +2,40 @@ import React from 'react';
 
 import './clock.css';
 
-const Clock = () => (
-  <div className="clock">
-    <div className="clock-center" />
-    <div className="clock-hand clock-hand-hour" />
-    <div className="clock-hand clock-hand-minute" />
-    <div className="clock-hand clock-hand-second" />
-  </div>
-);
+const calculateRotation = (divisor, value) => value * 360 / divisor;
+
+const makeStyle = (divisor, value) => ({
+  transform: `rotate(${calculateRotation(divisor, value)}deg)`
+});
+
+function Clock({ hours, minutes, seconds }) {
+  return (
+    <div className="clock">
+      <div className="clock-center" />
+      <div className="clock-hand clock-hand-hour" style={makeStyle(12, hours)} />
+      <div className="clock-hand clock-hand-minute" style={makeStyle(60, minutes)} />
+      <div className="clock-hand clock-hand-second" style={makeStyle(60, seconds)} />
+    </div>
+  );
+}
+
+const makeValidator = max => (props, propName, componentName) => {
+  const value = props[propName];
+
+  console.log(props, propName, componentName);
+
+  if (value === undefined || typeof value !== 'number' || value < 0 || value > max) {
+    return new Error(
+      `Invalid prop "${propName}" supplied to "${componentName}".
+       The value should be a number between 0 and ${max}`
+    );
+  }
+};
+
+Clock.propTypes = {
+  hours: makeValidator(12),
+  minutes: makeValidator(60),
+  seconds: makeValidator(60)
+};
 
 export default Clock;
