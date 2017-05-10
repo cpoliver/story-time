bb5f7682a3794d19024fda78622fc88226dd2589
Charles P. Oliver <charles.oliver.extern@holidaycheck.com>
2017-05-10 14:10:38 +0300

    Add interval to allow clock to display the time

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 
 import Clock from './components/clock';
 
+const getTime = () => {
+  const now = new Date();
+
+  return {
+    hours: now.getHours(),
+    minutes: now.getMinutes(),
+    seconds: now.getSeconds()
+  };
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getTime();
+  }
+
+  componentDidMount() {
+    this.tick = this.tick.bind(this);
+    this.setState({ intervalId: setInterval(this.tick, 500) });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  tick() {
+    this.setState({ ...getTime() });
+  }
+
   render() {
+    const { hours, minutes, seconds } = this.state;
+
     return (
       <div className="App">
-        <Clock hours={12} minutes={15} seconds={30} />
+        <Clock hours={hours} minutes={minutes} seconds={seconds} />
       </div>
     );
   }
